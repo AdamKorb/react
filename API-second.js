b026955535f73ffad29079000b2253390b3fd37c
AdamKorb <adam.korbel.16@gmail.com>
2024-02-09 13:02:56 +0100

    Create API-second.js

---
+++ API-second.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const url = "http://api.open-notify.org/iss-now.json"
+  const [latitude, setLatitude] = useState("")
+  const [longitude, setLongitude] = useState("")
+  const [urlMap, setUrlMap] = useState("")
+
+
+  const getCoordinates = async() => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setLatitude(data["iss_position"]["latitude"])
+    setLongitude(data["iss_position"]["longitude"])
+    const iss_long = data["iss_position"]["longitude"]
+    const iss_lat = data["iss_position"]["latitude"]
+    setUrlMap(`https://sk.mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}&z=5`)
+  }
+  
+  useEffect ( () => {
+    getCoordinates()
+  },[])
+
+  return (
+    <div>
+      <h1>Poloha stanice ISS:</h1>
+      <h2>zemepisná šírka</h2>
+      <p>{latitude}</p>
+      <h2>zemepisná dĺžka</h2>
+      <p>{longitude}</p>
+      <a href={urlMap} target='_blank'>Pozícia ISS v Mapách.cz</a>
+    </div>
+  )
+}
+
+
+export default App
